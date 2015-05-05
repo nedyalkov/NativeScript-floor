b676a763442b17f907ef023fb342218eda099e06
Miroslav Nedyalkov <miroslav.nedialkov@gmail.com>
2015-05-05 19:09:09 +0100

    Update config.js

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 exports.plansInfo = {
     demo: {
-        name: 'My floor plan',
+        name: 'My floorplan',
         id: '55468128ebfbd6b660588733',
         params: ['theme=professional', 'zoom=0.3', 'zones', 'furniture', 'walls']
     }
