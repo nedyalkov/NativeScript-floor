4a7ff22c4dfa8c7c1e03c689ca324ec291b1e1fb
Miroslav Nedyalkov <miroslav.nedialkov@gmail.com>
2015-05-27 11:11:15 +0100

    Use new APIs

--- app/config.js
+++ app/config.js
@@ -2,6 +2,6 @@ exports.plansInfo = {
     demo: {
         name: 'My floorplan',
         id: '55468128ebfbd6b660588733',
-        params: ['theme=professional', 'zoom=0.3', 'zones', 'furniture', 'walls']
+        params: ['theme=professional', 'width=750', 'legend=false', 'labels=false']
     }
 };
